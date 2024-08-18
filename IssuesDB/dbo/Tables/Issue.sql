287651319405cff039200fdf7937e91ebdc32386
Reynald Ortiga <keanortega09@gmail.com>
2024-08-19 02:33:30 +0800

    Add IssuesDB project in the solution

---
+++ IssuesDB/dbo/Tables/Issue.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[Issue] (
+    [Id]          INT           IDENTITY (1, 1) NOT NULL,
+    [Title]       VARCHAR (100) NOT NULL,
+    [Description] TEXT          NOT NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
