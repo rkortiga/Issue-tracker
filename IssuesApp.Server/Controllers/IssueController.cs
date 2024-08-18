850f3617d5089aebb29bcd16e31408737694ba2b
Reynald Ortiga <keanortega09@gmail.com>
2024-08-19 00:30:44 +0800

    Create IssueController

---
+++ IssuesApp.Server/Controllers/IssueController.cs
@@ -0,0 +1,82 @@
+using IssuesApp.Server.Models.Dto;
+using IssuesApp.Server.Service;
+using Microsoft.AspNetCore.Mvc;
+
+namespace IssuesApp.Server.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class IssueController : ControllerBase
+{
+	private readonly IIssueService _issueService;
+
+	public IssueController(IIssueService issueService)
+	{
+		_issueService = issueService;
+	}
+	
+	[HttpGet("Issues")]
+	public async Task<IActionResult> GetIssues()
+	{
+		var issues = await _issueService.GetAllIssuesAsync();
+		if (!issues.Any())
+		{
+			return NoContent();
+		}
+		return Ok(issues);
+	}
+	
+	[HttpGet("Issue/{id}")]
+	public async Task<IActionResult> GetIssueById(int id)
+	{
+		var issue = await _issueService.GetIssueByIdAsync(id);
+		if (issue == null)
+		{
+			return NotFound();
+		}
+		
+		return Ok(issue);
+	}
+	
+	[HttpPost("CreateIssue/")]
+	public async Task<IActionResult> CreateIssue(CreateIssueDto issue)
+	{
+		if(!ModelState.IsValid)
+		{
+			return BadRequest(ModelState);
+		}
+		
+		var newIssue = await _issueService.CreateIssueAsync(issue);
+		return CreatedAtAction(nameof(GetIssueById), new { id = newIssue.Id }, newIssue);
+	}
+	
+	[HttpPut("UpdateIssue/{id}")]
+	public async Task<IActionResult> UpdateIssue(UpdateIssueDto issue, int id)
+	{
+		if(!ModelState.IsValid)
+		{
+			return BadRequest(ModelState);
+		}
+		
+		var updatedIssue = await _issueService.UpdateIssueAsync(issue, id);
+		if (updatedIssue == null)
+		{
+			return NotFound();
+		}
+		
+		return Ok(updatedIssue);
+	}
+	
+	[HttpDelete("DeleteIssue/{id}")]
+	public async Task<IActionResult> DeleteIssue(int id)
+	{
+		var deletedIssue = await _issueService.DeleteIssueAsync(id);
+		
+		if (!deletedIssue)
+		{
+			return NotFound();
+		}
+		
+		return NoContent();
+	}
+}
