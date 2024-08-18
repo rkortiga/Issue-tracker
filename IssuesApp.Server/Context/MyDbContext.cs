e041e1d486eb507f65898c0af98ed93b02d3339e
Reynald Ortiga <keanortega09@gmail.com>
2024-08-19 00:13:09 +0800

    Inital commit

---
+++ IssuesApp.Server/Context/MyDbContext.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using IssuesApp.Server.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace IssuesApp.Server.Context;
+
+public partial class MyDbContext : DbContext
+{
+    public MyDbContext()
+    {
+    }
+
+    public MyDbContext(DbContextOptions<MyDbContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Issue> Issues { get; set; }
+    
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Issue>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__Issue__3214EC07905A5772");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
---
+++ IssuesApp.Server/Models/Issue.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+namespace IssuesApp.Server.Models;
+
+[Table("Issue")]
+public partial class Issue
+{
+    [Key]
+    public int Id { get; set; }
+
+    [StringLength(100)]
+    [Unicode(false)]
+    public string Title { get; set; } = null!;
+
+    [Column(TypeName = "text")]
+    public string Description { get; set; } = null!;
+}
