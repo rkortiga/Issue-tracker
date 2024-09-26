41b4b569e861b3207d013af5ad4cada0a6936152
Reynald Ortiga <keanortega09@gmail.com>
2024-09-27 07:15:33 +0800

    Fix issue-service test

--- issuesapp.client/src/app/services/issue.service.spec.ts
+++ issuesapp.client/src/app/services/issue.service.spec.ts
@@ -1,16 +1,19 @@
 import { TestBed } from '@angular/core/testing';
 
-import {IssueService} from './issue.service';
+import { IssueService } from './issue.service';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 describe('IssueService', () => {
-  let service: IssueService;
+    let service: IssueService;
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(IssueService);
-  });
+    beforeEach(() => {
+        TestBed.configureTestingModule({
+            imports: [HttpClientTestingModule]
+        });
+        service = TestBed.inject(IssueService);
+    });
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
-  });
+    it('should be created', () => {
+        expect(service).toBeTruthy();
+    });
 });
