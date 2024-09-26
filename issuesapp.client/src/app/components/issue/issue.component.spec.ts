706c51925b56e965306ed175b25810140ed489e9
Reynald Ortiga <keanortega09@gmail.com>
2024-09-27 07:13:49 +0800

    Fix issue component test

--- issuesapp.client/src/app/components/issue/issue.component.spec.ts
+++ issuesapp.client/src/app/components/issue/issue.component.spec.ts
@@ -1,23 +1,30 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { IssueComponent } from './issue.component';
+import { IssueService } from '../../services/issue.service';
+import { MessageService } from 'primeng/api';
+import { PaginatorModule } from 'primeng/paginator';
+import { PaginatorComponent } from '../paginator/paginator.component';
+import { TableModule } from 'primeng/table';
 
 describe('IssueComponent', () => {
-  let component: IssueComponent;
-  let fixture: ComponentFixture<IssueComponent>;
+    let component: IssueComponent;
+    let fixture: ComponentFixture<IssueComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [IssueComponent]
-    })
-    .compileComponents();
+    beforeEach(async () => {
+        await TestBed.configureTestingModule({
+            imports: [HttpClientTestingModule, PaginatorModule, TableModule],
+            declarations: [IssueComponent, PaginatorComponent],
+            providers: [IssueService, MessageService]
+        })
+        .compileComponents();
 
-    fixture = TestBed.createComponent(IssueComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+        fixture = TestBed.createComponent(IssueComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
 });
