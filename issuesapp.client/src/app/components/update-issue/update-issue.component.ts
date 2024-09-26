3c31307dfc5b6764c6527c07373e9e601febdb41
Reynald Ortiga <keanortega09@gmail.com>
2024-09-26 12:45:09 +0800

    Refactor update-issue component

--- issuesapp.client/src/app/components/update-issue/update-issue.component.ts
+++ issuesapp.client/src/app/components/update-issue/update-issue.component.ts
@@ -1,51 +1,85 @@
-import {Component, OnInit} from '@angular/core';
-import {Updateissuedto} from "../../models/updateissuedto";
-import {IssueService} from "../../services/issue.service";
-import {FormBuilder} from "@angular/forms";
-import {MessageService} from "primeng/api";
-import {ActivatedRoute, Router} from "@angular/router";
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Updateissuedto } from "../../models/updateissuedto";
+import { IssueService } from "../../services/issue.service";
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
+import { MessageService } from "primeng/api";
+import { ActivatedRoute, Router } from "@angular/router";
+import { Subject, takeUntil } from 'rxjs';
 
 @Component({
-  selector: 'app-update-issue',
-  templateUrl: './update-issue.component.html',
-  styleUrl: './update-issue.component.css'
+    selector: 'app-update-issue',
+    templateUrl: './update-issue.component.html',
+    styleUrl: './update-issue.component.css'
 })
-export class UpdateIssueComponent implements OnInit {
-  issue: Updateissuedto = {
-    id: 0,
-    title: '',
-    description: '',
-  };
+export class UpdateIssueComponent implements OnInit, OnDestroy {
+    updateIssueForm!: FormGroup;
+    private ngUnsubscribe$: Subject<void> = new Subject<void>();
 
-  constructor(private activatedRoute: ActivatedRoute,
-              private issueService: IssueService,
-              private formBuilder: FormBuilder,
-              private messageService: MessageService,
-              private router: Router) {}
+    constructor(
+        private activatedRoute: ActivatedRoute,
+        private issueService: IssueService,
+        private formBuilder: FormBuilder,
+        private messageService: MessageService,
+        private router: Router
+    ) {}
 
-  ngOnInit() {
-    this.activatedRoute.paramMap.subscribe({next: (params) => {
-        const id = params.get('id');
+    ngOnInit() {
+        this.buildForm();
+    }
 
-        if (id) {
-          this.issueService.getIssueById(parseInt(id)).subscribe({next: (res) => {
-            this.issue = res;
-            }});
+    ngOnDestroy() {
+        this.ngUnsubscribe$.next();
+        this.ngUnsubscribe$.complete();
+    }
+
+    buildForm() {
+        this.updateIssueForm = this.formBuilder.group({
+            id: [{value: null, disabled: true}, Validators.required],
+            title: ['', Validators.required],
+            description: ['', Validators.required]
+        });
+
+        this.activatedRoute.paramMap
+        .pipe(takeUntil(this.ngUnsubscribe$))
+        .subscribe(params => {
+            const id = params.get('id');
+            if (id) {
+                this.issueService.getIssueById(parseInt(id)).subscribe(res => {
+                    this.updateIssueForm.patchValue(res);
+                });
+            }
+        });
+    }
+
+    updateIssue() {
+        if (this.updateIssueForm.invalid) {
+            this.messageService.add({
+                severity: 'error',
+                summary: 'Error',
+                detail: 'Please fill out all required fields'
+            });
+            return;
         }
-      },
-    });
-  }
+        
+        const issueData: Updateissuedto = this.updateIssueForm.getRawValue();
+        this.issueService.updateIssue(issueData, issueData.id).pipe(takeUntil(this.ngUnsubscribe$))
+        .subscribe(res => {
+            this.messageService.add({
+                severity: 'success',
+                summary: 'Success',
+                detail: 'Issue updated successfully!'
+            });
+            this.router.navigate(['/issues']);
+        });
+    }
 
-  updateIssue() {
-    this.issueService.updateIssue(this.issue, this.issue.id).subscribe({next: (res) => {
-        this.messageService.add({severity: 'success', summary: 'Success', detail: 'Issue updated successfully!'});
-        this.router.navigate(['/issues']);
-    }});
-  }
-
-  delete() {
-    this.issueService.deleteIssue(this.issue.id).subscribe({next: (res) => {
-      this.router.navigate(['/issues']);
-      }});
-  }
+    delete() {
+        const issueId = this.updateIssueForm.get('id')?.value;
+        if (issueId) {
+            this.issueService.deleteIssue(issueId).pipe(takeUntil(this.ngUnsubscribe$))
+            .subscribe(() => {
+                this.router.navigate(['/issues']);
+            });
+        }
+    }
 }
