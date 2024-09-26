03c6937e74c8b0957b99a85658a51c80df62b033
Reynald Ortiga <keanortega09@gmail.com>
2024-09-27 06:58:57 +0800

    Fix update-issue test

--- issuesapp.client/src/app/components/update-issue/update-issue.component.spec.ts
+++ issuesapp.client/src/app/components/update-issue/update-issue.component.spec.ts
@@ -1,23 +1,43 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { UpdateIssueComponent } from './update-issue.component';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MenubarModule } from 'primeng/menubar';
+import { ActivatedRoute, Router } from '@angular/router';
+import { of } from 'rxjs';
+import { IssueService } from '../../services/issue.service';
+import { MessageService } from 'primeng/api';
 
 describe('UpdateIssueComponent', () => {
-  let component: UpdateIssueComponent;
-  let fixture: ComponentFixture<UpdateIssueComponent>;
+    let component: UpdateIssueComponent;
+    let fixture: ComponentFixture<UpdateIssueComponent>;
+    let mockRouter: jasmine.SpyObj<Router>;
+    let mockActivatedRoute: any;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [UpdateIssueComponent]
-    })
-    .compileComponents();
+    beforeEach(async () => {
+        mockRouter = jasmine.createSpyObj('Router', ['navigate']);
+        mockActivatedRoute = {
+            paramMap: of({get: (key: string) => '1'})
+        };
 
-    fixture = TestBed.createComponent(UpdateIssueComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+        await TestBed.configureTestingModule({
+            imports: [HttpClientTestingModule, ReactiveFormsModule, FormsModule, MenubarModule],
+            declarations: [UpdateIssueComponent],
+            providers: [
+                IssueService,
+                MessageService,
+                {provide: Router, useValue: mockRouter},
+                {provide: ActivatedRoute, useValue: mockActivatedRoute}
+            ]
+        })
+        .compileComponents();
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+        fixture = TestBed.createComponent(UpdateIssueComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
+
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
 });
